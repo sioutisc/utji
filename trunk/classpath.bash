5d18933e32c21db75fa90e0303df9d50cf5ea76a
christos.sioutis <christos.sioutis@d8fe815d-fbc0-1a0e-a6fc-c3d4022d7f28>
2010-08-17 13:17:31 +0000

    fixed classpath bash script

--- trunk/classpath.bash
+++ trunk/classpath.bash
@@ -1,7 +1,7 @@
 # ensure jack.jar is locared in your classpath already
 # or simply make a symbolic link to jack.jar in the lib directory
-for file in `find ./lib`
+for file in `ls ./lib`
 do
-  export CLASSPATH=$CLASSPATH:$file
+  export CLASSPATH=$CLASSPATH:./lib/$file
 done
 export CLASSPATH=$CLASSPATH:./dist/lib/utji.jar
